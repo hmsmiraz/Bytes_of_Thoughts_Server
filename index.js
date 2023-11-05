4f00703a23d8e9fa89448ebd4ae779164c3964e6
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-06 02:19:15 +0600

    added user db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.meftkqt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,24 +25,54 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     //await client.connect();
 
-    const blogCollection = client.db('botsDB').collection('blogs');
+    const blogCollection = client.db("botsDB").collection("blogs");
+    const userCollection = client.db("botsDB").collection("users");
 
-    //service 
-    app.get('/blogs', async(req, res)=>{
-        const cursor = blogCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    //service
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
-    app.post('/blogs', async (req, res) =>{
-        const blogs = req.body;
-        const result = await blogCollection.insertOne(blogs);
-        res.send(result);
-        console.log(result);
-      });
+    app.post("/blogs", async (req, res) => {
+      const blogs = req.body;
+      const result = await blogCollection.insertOne(blogs);
+      res.send(result);
+      console.log(result);
+    });
+
+    // users
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = {
+        $set: {
+          uid: user.uid,
+          lastLoggedAt: user?.lastLoggedAt,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     //await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -51,11 +80,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Bytes of Thoughts Blog server is Running...");
+});
 
-app.get('/', (req, res) =>{
-    res.send('Bytes of Thoughts Blog server is Running...')
-})
-
-app.listen(port, () =>{
-    console.log(`Bytes of Thoughts server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Bytes of Thoughts server is running on port: ${port}`);
+});
