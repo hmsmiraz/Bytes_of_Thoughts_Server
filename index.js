932ca9abb395894bc0334cf4903443343e10ae68
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-06 12:56:37 +0600

    added details blog

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.meftkqt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,6 +40,15 @@ async function run() {
       const result = await blogCollection.insertOne(blogs);
       res.send(result);
       console.log(result);
+    });
+
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+      const result = await blogCollection.findOne(query);
+      console.log(result);
+      res.send(result);
     });
 
     // users
